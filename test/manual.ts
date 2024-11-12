efdd2c1960e9ef5261f7041a89f64f8e63145046
soul <opensource@aerrobert.com>
2024-11-11 22:07:05 -0800

    Mon Nov 11 22:07:05 PST 2024

--- test/manual.ts
+++ test/manual.ts
@@ -4,7 +4,7 @@ import { S3DataStorage } from '../src/implementations/storage/s3';
 
 const intelegence = new Intelegence({
     image: new OpenAIImageModel({
-        apiKey: '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',
+        apiKey: '',
     }),
     dataStore: new S3DataStorage({
         bucketName: 'aivideogenerator-405505053377-prod-job-data-bucket',
